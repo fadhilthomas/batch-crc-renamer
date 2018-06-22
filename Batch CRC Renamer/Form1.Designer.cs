672edfc0081c0923f0a58ac63032141331a5803b
Muhammad Thomas Fadhila Yahya <mthomasfadhilayahya@gmail.com>
2018-06-23 06:01:29 +0700

    may23rd2018

---
+++ Batch CRC Renamer/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Batch_CRC_Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cRCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkRenameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LV1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.sl1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.sl2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.cRCToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(552, 26);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(43, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // cRCToolStripMenuItem
+            // 
+            this.cRCToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkToolStripMenuItem,
+            this.checkRenameToolStripMenuItem});
+            this.cRCToolStripMenuItem.Name = "cRCToolStripMenuItem";
+            this.cRCToolStripMenuItem.Size = new System.Drawing.Size(44, 22);
+            this.cRCToolStripMenuItem.Text = "CRC";
+            // 
+            // checkToolStripMenuItem
+            // 
+            this.checkToolStripMenuItem.Name = "checkToolStripMenuItem";
+            this.checkToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.checkToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.checkToolStripMenuItem.Text = "Check";
+            this.checkToolStripMenuItem.Click += new System.EventHandler(this.checkToolStripMenuItem_Click);
+            // 
+            // checkRenameToolStripMenuItem
+            // 
+            this.checkRenameToolStripMenuItem.Name = "checkRenameToolStripMenuItem";
+            this.checkRenameToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.R)));
+            this.checkRenameToolStripMenuItem.Size = new System.Drawing.Size(238, 22);
+            this.checkRenameToolStripMenuItem.Text = "Check and Rename";
+            this.checkRenameToolStripMenuItem.Click += new System.EventHandler(this.checkRenameToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(58, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // LV1
+            // 
+            this.LV1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LV1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LV1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.LV1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LV1.FullRowSelect = true;
+            this.LV1.GridLines = true;
+            this.LV1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.LV1.Location = new System.Drawing.Point(0, 29);
+            this.LV1.Name = "LV1";
+            this.LV1.Size = new System.Drawing.Size(552, 325);
+            this.LV1.TabIndex = 6;
+            this.LV1.UseCompatibleStateImageBehavior = false;
+            this.LV1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File Name";
+            this.columnHeader1.Width = 443;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CRC";
+            this.columnHeader2.Width = 101;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sl1,
+            this.sl2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 357);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(552, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // sl1
+            // 
+            this.sl1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sl1.Name = "sl1";
+            this.sl1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // sl2
+            // 
+            this.sl2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sl2.Name = "sl2";
+            this.sl2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 379);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.LV1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fredrica - Batch CRC Renamer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cRCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem checkRenameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ListView LV1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel sl1;
+        private System.Windows.Forms.ToolStripStatusLabel sl2;
+
+
+    }
+}
+
---
+++ Batch CRC Renamer/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using DamienG.Security.Cryptography;
+// Created by Muhammad Thomas Fadhila Yahya
+// mthomasfadhilayahya@gmail.com
+
+
+namespace Batch_CRC_Renamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        //inialisasi var
+        int jumlah = 0;
+        Crc32 crc32 = new Crc32();
+        String hash = String.Empty;
+
+        private void cekCRC(ListView LV1, int jumlah, ToolStripStatusLabel sl1)
+        {
+            //fungsi untuk memeriksa CRC file
+
+            hash = String.Empty;
+
+            for (int j = 0; j < jumlah; j++)
+            {
+                using (FileStream fs = File.Open(sl1 + LV1.Items[j].SubItems[0].Text, FileMode.Open))
+                    foreach (byte b in crc32.ComputeHash(fs)) hash += b.ToString("x2").ToUpper();
+                LV1.Items[j].SubItems[1].Text = hash;
+                LV1.Refresh();
+                hash = String.Empty;
+            }
+        }
+
+        private String cekeks(ListView LV1, int j, int titik)
+        {
+            //fungsi untuk menghasilkan string ekstensi
+
+            if (titik > 0)
+            {
+                int panjang = LV1.Items[j].SubItems[0].Text.Length;
+                return LV1.Items[j].SubItems[0].Text.Remove(0, titik);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private String namatpeks(ListView LV1, int titik, int j, ToolStripStatusLabel sl1)
+        {
+            //fungsi untuk menghasilkan string nama file tanpa ekstensi
+
+            if (titik > 0)
+            {
+                return sl1 + LV1.Items[j].SubItems[0].Text.Remove(titik);
+            }
+            else
+            {
+                return sl1 + LV1.Items[j].SubItems[0].Text;
+            }
+        }
+
+        private void cekRename(ListView LV1, int jumlah, ToolStripStatusLabel sl1)
+        {
+            //fungsi untuk memeriksa CRC dan melakukan Rename
+
+            if (LV1.Items[0].SubItems[1].Text != "")
+            {
+                for (int j = 0; j < jumlah; j++)
+                {
+                    int titik = LV1.Items[j].SubItems[0].Text.LastIndexOf('.');
+                    string ext = cekeks(LV1, j, titik);
+                    string nama = sl1 + LV1.Items[j].SubItems[0].Text;
+                    string namaeks = namatpeks(LV1, titik, j, sl1);
+                    File.Move(nama, namaeks + " [" + LV1.Items[j].SubItems[1].Text + "]" + ext);
+                }
+            }
+            else
+            {
+                cekCRC(LV1, jumlah, sl1);
+                cekRename(LV1, jumlah, sl1);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //keluar
+            Application.ExitThread();
+        }
+
+        public void AddRow(ListView lvw, string[] items)
+        {
+            //item utama
+            ListViewItem new_item = lvw.Items.Add(items[0]);
+
+            //sub-items
+            for (int i = 1; i < items.Length; i++)
+                new_item.SubItems.Add(items[i]);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //clear listview
+                LV1.Items.Clear();
+
+                //mengambil path
+                string fullPath = openFileDialog1.FileName;
+                string fileName = openFileDialog1.SafeFileName;
+                string path = fullPath.Remove(fullPath.Length - fileName.Length);
+
+                sl1.Text = path;
+                int i = 1;
+                foreach(string s in openFileDialog1.SafeFileNames){
+                    AddRow(LV1,new string[] {s,""});
+                    i++;
+                }
+
+                //menampilkan jumlah file
+                sl2.Text = "   [ " + (i - 1).ToString() + " files ]";
+                jumlah = i - 1;
+            }
+        }
+
+        private void checkToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cekCRC(LV1, jumlah, sl1);
+        }
+
+        private void checkRenameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cekRename(LV1, jumlah, sl1);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //profil
+            MessageBox.Show("Created by\n\nMuhammad Thomas Fadhila Yahya\nJuly 1st, 2017", "About", MessageBoxButtons.OK,
+        MessageBoxIcon.Information);
+        }
+
+    }
+}
